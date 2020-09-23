359dfb3230c21001c94a3302c8e544e41a3386ac
Nelson Jr <juniordell123@gmail.com>
2020-09-22 23:47:40 -0300

    update Show component

--- src/components/Show/index.jsx
+++ src/components/Show/index.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { useFetch } from '../../hooks/useFetch'
 
-import { Container, ImageContent, Title } from './styles'
+import { Container, Content, ImageContent, Title } from './styles'
 
 function Show() {
   const { data } = useFetch('filter.php?c=Cocktail')
 
   return (
     <Container>
-      <Title>Content</Title>
-      <ImageContent>
-        <img
-          style={{ width: '400px' }}
-          src={data?.data.drinks[49].strDrinkThumb}
-          alt=''
-        />
-      </ImageContent>
+      <Content>
+        <Title>Content</Title>
+        <ImageContent>
+          <img
+            style={{ width: '400px' }}
+            src={data?.data.drinks[49].strDrinkThumb}
+            alt=''
+          />
+        </ImageContent>
+      </Content>
     </Container>
   )
 }
--- src/components/Show/styles.js
+++ src/components/Show/styles.js
@@ -3,6 +3,11 @@ import styled from 'styled-components'
 export const Container = styled.div`
   height: 100vh;
   background: #013620;
+  width: 100%;
+`
+
+export const Content = styled.div`
+  height: 100vh;
   max-width: 1240px;
   margin: 0 auto;
   display: flex;
@@ -17,7 +22,12 @@ export const Title = styled.h1`
 `
 
 export const ImageContent = styled.div`
+  border-radius: 20px;
   border: 2px solid green;
   width: 400px;
   height: 400px;
+
+  > img {
+    border-radius: 20px;
+  }
 `
